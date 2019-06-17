ad17ebddd5679b2063c5ebbac9489a992e9c24bc
i.naoto <e.h0andwhere@gmail.com>
2019-06-17 18:48:55 +0900

    ログイン処理追加

--- src/login.php
+++ src/login.php
@@ -1,4 +1,62 @@
 <?php
+include 'function.php';
+
+error_log('ログイン画面');
+
+// ログイン認証
+
+
+if (!empty($_POST)) {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // 入力項目のバリデーションチェック
+
+  //emailの文字数チェック
+  //emailの形式チェック
+  //passの文字数チェック
+  //passの文字チェック
+
+  if (empty($errMsg)) {
+    // DB接続してユーザあるかチェック
+    // あればログイン、なければTOPページに遷移
+    try {
+      $dbh = dbConnect();
+      $sql = 'SELECT * FROM users WHERE email = :email AND password = :password';
+      $data = array(':email' => $email, ':password' => $password);
+      $stmt = queryPost($dbh, $sql, $data);
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      if (!empty($result)) {
+        // ログイン成功
+        error_log('ログイン成功');
+        $_SESSION['login_id'] = $result['id'];
+        $_SESSION['login_email'] = $result['email'];
+        $_SESSION['login_date'] = time();
+
+        error_log('セッション情報:' . print_r($_SESSION, true));
+
+        header('Location:myPage.php');
+        exit;
+      }
+      else {
+        // ログイン失敗
+        error_log('ログイン失敗');
+        session_destroy();
+        header('Location:index.php');
+        exit;
+      }
+    }
+    catch(Exception $e) {
+      error_log('エラー発生' . $e->getMessage());
+      $errMsg['common'] = '例外発生';
+    }
+  }
+}
+
+?>
+
+<?php
 $title = 'ログイン';
 include 'header.php';
 include 'bodyHeader.php';
@@ -6,8 +64,8 @@ include 'bodyHeader.php';
   <main>
     <div class="main-form">
       <h1>Login</h1>
-      <form action="#">
-        <input type="text" name="user_name" placeholder="UserName" class="input-width-max">
+      <form action="" method="post">
+        <input type="text" name="email" placeholder="Email" class="input-width-max">
         <input type="password" name="password" placeholder="Password" class="input-width-max">
         <input type="submit" value="Enter" class="input-width-max">
       </form>
