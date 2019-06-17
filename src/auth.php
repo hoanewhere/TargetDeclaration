ce9ea5b353e4296822eb75deb3f8c43fd4bfe158
i.naoto <e.h0andwhere@gmail.com>
2019-06-17 19:58:06 +0900

    ログイン画面遷移時にログイン画面に遷移するループを回避する処理を追加

--- src/auth.php
+++ src/auth.php
@@ -1,16 +1,22 @@
 <?php
 error_log('ログイン認証開始');
 
-if (!isset($_SESSION['login_id'])) {
-  // ログイン認証失敗
-  error_log('ログイン認証失敗');
-  session_destroy();
-  header('Location:login.php');
-  exit;
+$current_url = basename($_SERVER['REQUEST_URI']);
+error_log('カレントURI:' . $current_url);
+
+if ($current_url != 'login.php') {
+  if (!isset($_SESSION['login_id'])) {
+    // ログイン認証失敗
+    error_log('ログイン認証失敗');
+    session_destroy();
+    header('Location:login.php');
+    exit;
+  }
+  else {
+    // ログイン認証成功
+    error_log('ログイン認証成功');
+  }
 }
-else {
-  // ログイン認証成功
-  error_log('ログイン認証成功');
-}
+
 
 ?>
